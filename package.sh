a347dd257f78fa0496b9af92d1520e312581edcb
Joseph Freeston <snorklerjoe@gmail.com>
2022-08-22 22:19:32 -0400

    bug fix

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-zip -r cubeserver-api-circuitpython.zip . -x .git/* -x .gitignore 2>&1 > /dev/null
+zip -r cubeserver-api-circuitpython.zip . -x .git -x .gitignore 2>&1 > /dev/null
 echo cubeserver-api-circuitpython.zip
