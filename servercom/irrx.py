ab50fc53f524dfdc97c43b902bb8b7b74184bc3c
Joseph Freeston <snorklerjoe@gmail.com>
2023-04-30 01:17:33 -0400

    Make irrx use timeout

--- servercom/irrx.py
+++ servercom/irrx.py
@@ -6,6 +6,8 @@ import pulseio
 import board
 import digitalio
 import adafruit_irremote
+from .timeout import Timeout
+
 #irPin = digitalio.DigitalInOut(board.A0)
 #irPin.direction = digitalio.Direction.INPUT
 DEFAULT_PULSEIN = pulseio.PulseIn(board.A0, maxlen=30000, idle_state=True)
@@ -45,44 +47,60 @@ class IRMsg:
     def text(self):
         return self.body.decode()
 
-def receive(decoder = DEFAULT_DECODER, pulsein = DEFAULT_PULSEIN):
-    # Read until BELL
-    print("*", end="")
-    while decode_chunk(decoder, pulsein) != b'\x07':
-        pass
-    buf = b'\x07'
-    # Read until no BELL
-    print("*", end="")
-    while buf == b'\x07':
-        buf = decode_chunk(decoder, pulsein)
-    # Figure out datagram length
-    print("*", end="")
-    total_length = int.from_bytes(buf, 'big')
-    read_length = 0
-    if total_length < 8:
-        return b'', b'', b''
-    # Start receiving the datagram
-    print("*", end="")
-    protocol = decode_line(decoder, pulsein)
-    read_length += len(protocol)
-    if protocol != b"CSMSG/1.0\r\n":
-        print("Potentially unsupported protocol version. Try getting the latest servercom library?")
-    headers = {}
-    buf = b': '
-    while buf.find(b': ') >= 0 or buf == b'\r\n':
+def receive(timeout: int = 10, line_timeout: int = 100, decoder = DEFAULT_DECODER, pulsein = DEFAULT_PULSEIN) -> IRMsg:
+
+    @Timeout(timeout)
+    def wait_for_bell():
+        # Read until BELL
         print("*", end="")
-        buf = decode_line(decoder, pulsein)
-        read_length += len(buf)
-        if buf == b'\r\n':
-            break
-        split_buf = buf.strip(b'\r\n').split(b': ')
-        headers.update({split_buf[0]: split_buf[1]})
-    body = b''
-    content_length = int(headers[b'Content-Length'])
-    while len(body) < content_length:
+        while decode_chunk(decoder, pulsein) != b'\x07':
+            t_wd.reset()
+        return True
+
+    if not wait_for_bell():
+        return None
+
+    t_wd = Timeout(line_timeout)
+    @t_wd
+    def receive_the_thing():
+        buf = b'\x07'
+        # Read until no BELL
         print("*", end="")
-        body += decode_chunk(decoder, pulsein)
-    return IRMsg(headers, body)
+        while buf == b'\x07':
+            buf = decode_chunk(decoder, pulsein)
+            t_wd.reset()
+        # Figure out datagram length
+        print("*", end="")
+        total_length = int.from_bytes(buf, 'big')
+        read_length = 0
+        if total_length < 8:
+            return None
+        # Start receiving the datagram
+        print("*", end="")
+        protocol = decode_line(decoder, pulsein)
+        t_wd.reset()
+        read_length += len(protocol)
+        if protocol != b"CSMSG/1.0\r\n":
+            print("Potentially unsupported protocol version. Try getting the latest servercom library?")
+        headers = {}
+        buf = b': '
+        while buf.find(b': ') >= 0 or buf == b'\r\n':
+            print("*", end="")
+            buf = decode_line(decoder, pulsein)
+            t_wd.reset()
+            read_length += len(buf)
+            if buf == b'\r\n':
+                break
+            split_buf = buf.strip(b'\r\n').split(b': ')
+            headers.update({split_buf[0]: split_buf[1]})
+        body = b''
+        content_length = int(headers[b'Content-Length'])
+        while len(body) < content_length:
+            print("*", end="")
+            body += decode_chunk(decoder, pulsein)
+            t_wd.reset()
+        return IRMsg(headers, body)
+    return receive_the_thing()
 
 #dude = receive()
 #print()
