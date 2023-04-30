de0eab4593b9f3ec3ffc9d46c5ca9f4c8a4bc19e
Joseph Freeston <snorklerjoe@gmail.com>
2023-04-30 00:14:34 -0400

    Added timeout library

--- package.sh
+++ package.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+git submodule update
+
 ./update_version.sh
 zip cubeserver-api-python.zip -r lib/ boot.py code.py 2>&1 > /dev/null
 echo cubeserver-api-python.zip
--- update_version.sh
+++ update_version.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+git submodule update
+
 VERSION=$(cat ./version.txt)
 VERSION_STR="__version__ = \"$VERSION\""
 
