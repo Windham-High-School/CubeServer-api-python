ba26a79c509a541a26b2546dae9bcd6857ae975a
Brian Morgan <brian.s.morgan@gmail.com>
2023-11-06 19:08:57 -0500

    add core libraries needed by all

--- package.sh
+++ package.sh
@@ -2,6 +2,9 @@
 
 git submodule update --remote --init --recursive
 
+wget https://raw.githubusercontent.com/adafruit/Adafruit_CircuitPython_IRRemote/main/adafruit_irremote.py -O ./lib/adafruit_irremote.py
+wget https://raw.githubusercontent.com/adafruit/Adafruit_CircuitPython_BMP280/main/adafruit_bmp280.py -O ./lib/adafruit_bmp280.py
+
 ./update_version.sh
 zip cubeserver-api-python.zip -r lib/ boot.py code.py 2>&1 > /dev/null
 echo cubeserver-api-python.zip
