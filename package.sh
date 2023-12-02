3bb583db3cc5eb972966433457aacdf64d1fd31a
Brian Morgan <brian.s.morgan@gmail.com>
2023-12-01 19:23:21 -0500

    Update package.sh
    Signed-off-by: Brian Morgan <brian.s.morgan@gmail.com>

--- package.sh
+++ package.sh
@@ -4,6 +4,7 @@ git submodule update --remote --init --recursive
 
 wget https://raw.githubusercontent.com/adafruit/Adafruit_CircuitPython_IRRemote/main/adafruit_irremote.py -O ./lib/adafruit_irremote.py
 wget https://raw.githubusercontent.com/adafruit/Adafruit_CircuitPython_BMP280/main/adafruit_bmp280.py -O ./lib/adafruit_bmp280.py
+wget https://raw.githubusercontent.com/adafruit/Adafruit_CircuitPython_NeoPixel/main/neopixel.py -O ./lib/neopixel.py
 
 ./update_version.sh
 zip cubeserver-api-python.zip -r lib/ boot.py code.py 2>&1 > /dev/null
